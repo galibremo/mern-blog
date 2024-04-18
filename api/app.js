a791b8077607e9df4dccccab8d9f2ebdb13f9e79
galibremo <galibremo@gmail.com>
2024-04-19 00:30:14 +0600

    Create Signup api

--- api/app.js
+++ api/app.js
@@ -1,8 +1,11 @@
 import express from "express";
-import userRouter from "./routes/user.routes.js";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
+app.use(express.json());
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 export default app;

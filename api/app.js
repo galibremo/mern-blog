7ee7c4e56cdcd8abf793d0b1783d1273f0f5963f
galibremo <galibremo@gmail.com>
2024-04-19 00:48:57 +0600

    Add Middleware to Handle Errors

--- api/app.js
+++ api/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import errorHandler from "./middleware/error.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use(errorHandler);
 
 export default app;

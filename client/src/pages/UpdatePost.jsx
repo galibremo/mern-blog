7c03f282292e2ae462ccea6f5204e106ed8efab3
galibremo <galibremo@gmail.com>
2024-05-07 13:19:09 +0600

    Update Update Post Page and Its Functionality

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
+import { Button, FileInput, Select, TextInput } from "flowbite-react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import {
@@ -6,6 +6,7 @@ import {
   getStorage,
   ref,
   uploadBytesResumable,
+  deleteObject,
 } from "firebase/storage";
 import { app } from "../firebase";
 import { useState, useEffect } from "react";
@@ -17,13 +18,17 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 
 export default function UpdatePost() {
+  const { postId } = useParams();
+  const storage = getStorage(app);
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
-  const [formData, setFormData] = useState({});
-  const { postId } = useParams();
-  const { currentUser } = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({
+    _id: postId,
+  });
 
+  console.log(formData);
   const navigate = useNavigate();
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,13 +46,14 @@ export default function UpdatePost() {
     fetchData();
   }, [postId]);
 
+  console.log(formData);
+
   const handleUpdloadImage = async () => {
     try {
       if (!file) {
         toast.error("Please select an image");
         return;
       }
-      const storage = getStorage(app);
       const fileName = new Date().getTime() + "-" + file.name;
       const storageRef = ref(storage, fileName);
       const uploadTask = uploadBytesResumable(storageRef, file);
@@ -59,7 +65,7 @@ export default function UpdatePost() {
           setImageUploadProgress(progress.toFixed(0));
         },
         (error) => {
-          setImageUploadError("Image upload failed");
+          toast.error("Image upload failed");
           setImageUploadProgress(null);
         },
         () => {
@@ -89,9 +95,18 @@ export default function UpdatePost() {
       }
     } catch (error) {
       toast.error(error.message);
+      const desertRef = ref(storage, formData.image);
+      deleteObject(desertRef);
     }
   };
 
+  const handleChange = async (e) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+  };
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">Update Post</h1>
@@ -104,14 +119,11 @@ export default function UpdatePost() {
             id="title"
             className="flex-1"
             value={formData?.title}
-            onChange={(e) =>
-              setFormData({ ...formData, title: e.target.value })
-            }
+            onChange={handleChange}
           />
           <Select
-            onChange={(e) =>
-              setFormData({ ...formData, category: e.target.value })
-            }
+            onChange={handleChange}
+            id="category"
             value={formData?.category}
           >
             <option value="uncategorized">Select a category</option>
@@ -146,9 +158,9 @@ export default function UpdatePost() {
             )}
           </Button>
         </div>
-        {formData && (
+        {formData.image && (
           <img
-            src={formData?.image}
+            src={formData.image}
             alt="upload"
             className="w-full h-72 object-cover"
           />
